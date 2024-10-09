690dfba2c885a6b54d16ef8dc2f90926ebf1f4b4
Mrepic229 <jis69508@gmail.com>
2024-10-09 14:17:49 -0500

    batman commit

---
+++ src/Card.java
@@ -0,0 +1,23 @@
+public class Card {
+
+    enum Suit {
+        CLUBS,
+        SPADES,
+        DIAMONDS,
+        HEARTS
+    }
+
+    enum Value {
+        
+    }
+
+    private Suit suit;
+    private char value;
+
+    public Card() {
+
+    }
+
+}
+
+ 
