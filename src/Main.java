26db7055b2494f63d97fc0e03ead76c85aeeb073
Mrepic229 <jis69508@gmail.com>
2024-10-11 14:40:17 -0500

    Placed code that was used in testing into examples()

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        //examples();
+    }
+
+    static void examples() {
         Deck myDeck = new Deck();
+        Player joe = new Player();
 
         myDeck.shuffle();
-        myDeck.printDeck();
+        //myDeck.printDeck();
+        joe.drawCard(myDeck);
+        joe.drawCard(myDeck);
+        joe.drawCard(myDeck);
+        joe.printHand();
     }
     
 }
