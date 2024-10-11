221568816e363f915feba9ea13191aaa1ccf90f2
Mrepic229 <jis69508@gmail.com>
2024-10-11 14:37:07 -0500

    added popTop() to Deck.java

--- src/Deck.java
+++ src/Deck.java
@@ -1,33 +1,40 @@
 import java.util.*;
-import java.io.*;
 
 public class Deck {
-    ArrayList<Card> Deck;
+    private ArrayList<Card> deck;
 
     public Deck() {
-        Deck = new ArrayList<Card>();
+        deck = new ArrayList<Card>();
         for (int i = 0; i < Suit.values().length; i++) {
             for (int j = 0; j < Value.values().length; j++) {
-                Deck.add(new Card(Suit.values()[i], Value.values()[j]));
+                deck.add(new Card(Suit.values()[i], Value.values()[j]));
             }
         }
     }
 
     public void shuffle() {
         for (int i = 0; i < 100; i++) {
-            switchTwoCards((int)(Math.random() * Deck.size()), (int)(Math.random() * Deck.size()));
+            switchTwoCards((int)(Math.random() * deck.size()), (int)(Math.random() * deck.size()));
         }
     }
 
     public void printDeck() {
-        for (int i = 0; i < Deck.size(); i++) {
-            Deck.get(i).printCard();
+        for (int i = 0; i < deck.size(); i++) {
+            deck.get(i).printCard();
         }
     }
     
     void switchTwoCards(int index1, int index2) {
-        Card temp = Deck.get(index1);
-        Deck.set(index1, Deck.get(index2));
-        Deck.set(index2, temp);
+        Card temp = deck.get(index1);
+        deck.set(index1, deck.get(index2));
+        deck.set(index2, temp);
     }
+
+    public Card popTop() {
+        Card result = deck.get(deck.size()-1);
+        deck.remove(deck.size()-1);
+
+        return result;
+    }
+
 }
